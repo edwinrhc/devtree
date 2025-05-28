1a0602dba4f2f1ae11404807c6a213539fcd2d1c
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-05-28 00:08:36 -0500

    feat User

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,9 +3,9 @@ import colors from 'colors';
 
 console.log(process.env.MONGODB_URI);
 
-export const connectDB = async () => {
+export const connectDB = async ()  => {
     try {
-        const {connection } = await mongoose.connect(process.env.MONGO_URI);
+        const {connection} = await mongoose.connect(process.env.MONGO_URI);
         const url = `${connection.host}:${connection.port}`;
         console.log(colors.cyan.bold(`MongoDB Conectado ${url}`));
     } catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import server from './server'
 
 
 const port =  process.env.PORT || 4000
+
 server.listen(port, () => {
     console.log(colors.blue.bold(`Servidor Funcionando en el puerto. ${port}`));
 })
