08bcb39be82f3201867849fc371eaa76ea54a032
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-05-29 23:45:39 -0500

    feat: Evitar usuarios duplicados, instalar version 4.17.22 @types/express y quitar la version 5

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,12 +1,21 @@
-import type {Request,Response} from 'express'
+import type {Request, Response} from 'express'
 import User from "../models/User";
 
 
-export const createAccount = async (req: Request,res: Response)=> {
+export const createAccount = async (req: Request, res: Response) => {
+
+    const {email} = req.body;
+
+    const userExtis = await User.findOne({email})
+    if (userExtis) {
+        const error = new Error('El usuario ya esta registrado ');
+        return res.status(409).json({error: error})
+    }
+    
     const user = new User(req.body);
     await user.save();
     // Finalizamos
-    res.send('Registro Creado correctamente');
+    res.status(201).send('Registro Creado correctamente');
 }
 
 
