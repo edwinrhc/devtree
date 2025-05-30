9761129baa17d5c5a48e71660117d8d826ccc1fa
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-05-29 23:58:23 -0500

    feat: Hashear password de usuarios

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,18 +1,22 @@
 import type {Request, Response} from 'express'
 import User from "../models/User";
+import {hashPassword} from "../utils/auth";
 
 
 export const createAccount = async (req: Request, res: Response) => {
 
-    const {email} = req.body;
+    const {email,password} = req.body;
 
     const userExtis = await User.findOne({email})
     if (userExtis) {
         const error = new Error('El usuario ya esta registrado ');
         return res.status(409).json({error: error})
     }
-    
+
     const user = new User(req.body);
+   const hash = await hashPassword(password);
+    console.log('hash', hash);
+
     await user.save();
     // Finalizamos
     res.status(201).send('Registro Creado correctamente');
