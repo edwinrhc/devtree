e1b50740d1bbe415baa5f1857c9915276f6382d0
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-05-30 00:33:32 -0500

    feat: Añadiendo validación al Registro de usuarios

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,17 @@
 import type {Request, Response} from 'express'
+import {validationResult} from "express-validator";
 import slug from 'slug'
 import User from "../models/User";
 import {hashPassword} from "../utils/auth";
 
 
 export const createAccount = async (req: Request, res: Response) => {
+
+    //TODO: Manejar errores
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
 
     const {email,password} = req.body;
 
--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,20 @@
 import {Router} from 'express';
+import {body} from 'express-validator';
 import {createAccount} from "./handlers";
 // Routing
 const router = Router();
 
 /** Autenticacón y registro **/
-router.post('/auth/register', createAccount)
+router.post('/auth/register',
+    body('handle').notEmpty().withMessage('Handle es requerido'),
+    body('name').notEmpty().withMessage('El nombre es requerido'),
+    body('email').isEmail().withMessage('El correo es requerido'),
+    body('password').isLength({min:8}).withMessage('Password debe tener minimo 8 digitos'),
+
+    createAccount)
 
 
-
-export  default router;
+export default router;
 
 
 
