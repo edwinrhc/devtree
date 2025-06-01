3faa599ce2d233aaa868a75fdbb41cc55cc1d771
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-06-01 00:29:08 -0500

    feat: reafactorizando

--- frontend/src/components/ErrorMessage.tsx
+++ frontend/src/components/ErrorMessage.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export default function ErrorMessage({children}:{children: React.ReactNode}) {
     return (
-        <p className="bg-red-50 text-red-60 p-3 uppercase text-sm font-bold">{children}</p>
+        <p className="bg-red-50 text-red-600 p-3 uppercase text-sm font-bold">{children}</p>
     )
 }
