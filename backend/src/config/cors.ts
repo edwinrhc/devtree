aa8687f769a42f38fd6faf189da0efbbedda3f32
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-06-01 23:53:07 -0500

    fix: Ajustando el backend para permitir peticiones localmente (PostMan, otros) y tambien peticiones desde el React,

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,9 +1,19 @@
 
 import { CorsOptions } from 'cors'
 
+console.log(process.argv)
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback){
-        if(origin == process.env.FRONTEND_URL){
+
+        const whiteList = [process.env.FRONTEND_URL]
+
+        //Aqui se ejecuta localmente
+        if(process.argv[2] === '--api'){
+            whiteList.push(undefined)
+        }
+
+        // Aqui se ejecuta con el frontend
+        if(whiteList.includes(origin)){
             callback(null, true);
         }else{
             callback(new Error('Error de cors'));
