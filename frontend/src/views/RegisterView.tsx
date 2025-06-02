f1f99b5e89efef3a05482a8a83abc58e5d1fdd52
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-06-01 21:13:50 -0500

    refactor: Creando un cliente de Axios para realizar peticiones URL

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,8 +1,10 @@
 import {Link} from "react-router-dom";
 import {useForm} from "react-hook-form"
-import axios, { isAxiosError} from 'axios'
+import  { isAxiosError} from 'axios'
+import {toast} from 'sonner'
 import  type { RegisterForm } from "../types"
 import ErrorMessage from "../components/ErrorMessage";
+import api from '../config/axios'
 
 
 export default function RegisterView() {
@@ -15,7 +17,7 @@ export default function RegisterView() {
         password_confirmation:'',
     }
 
-    const { register, watch, handleSubmit, formState: { errors } } =
+    const { register, watch,reset, handleSubmit, formState: { errors } } =
         useForm({ defaultValues: initialValues });
 
 
@@ -23,11 +25,12 @@ export default function RegisterView() {
 
     const handleRegister = async (formData: RegisterForm) => {
         try{
-            const {data} = await axios.post('http://localhost:4000/auth/register',formData)
-            console.log(data)
+            const {data} = await api.post(`/auth/register`,formData)
+            toast.success(data)
+            reset()
         }catch(error){
            if(isAxiosError(error) && error.response){
-               console.log(error.response?.data.error)
+               toast.error(error.response?.data.error)
            }
         }
     }
